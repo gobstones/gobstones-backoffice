a61009b95d72e20e1a64dbbdfc46d3bc9d3f0f1e
Juan Escalada <jf.escalada92@gmail.com>
2020-01-25 17:34:55 -0300

    Adding bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { getPersistor } from "@rematch/persist";
 import { PersistGate } from "redux-persist/es/integration/react";
 import App from "./components/App";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
