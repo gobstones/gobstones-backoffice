36b1cedd5593b6b11e2cf4bf6b9709987fcec051
Juan Escalada <jf.escalada92@gmail.com>
2020-01-27 01:26:45 -0300

    Using heroku api mock

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 export default {
-  API_URL: "http://localhost:3001",
+  API_URL: "https://gobstones-backend-api-mock.herokuapp.com",
   GOBSTONES_URL: "https://gobstones.github.io/gobstones-web"
 };
